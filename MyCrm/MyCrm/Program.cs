d36e21cc57a13789e3e26e9cf195c419f33408ec
rousojohn <rousojohn@gmail.com>
2017-07-06 23:31:26 +0300

    Additional Commit
    Additional commit to previous

--- MyCrm/MyCrm/Program.cs
+++ MyCrm/MyCrm/Program.cs
@@ -22,6 +22,29 @@ namespace MyCrm
         [STAThread]
         static void Main()
         {
+            appSettings.Read();
+
+            var settings2 = new MongoClientSettings
+            {
+                Server = new MongoServerAddress("localhost", 27017),
+                UseSsl = false
+            };
+            var client2 = new MongoClient(settings2);
+
+            IMongoDatabase db = client2.GetDatabase("myTest");
+
+            var collection1 = db.GetCollection<User>("test");
+            var collection2 = db.GetCollection<BsonDocument>("test2");
+
+            collection1.DeleteMany(FilterDefinition<User>.Empty);
+            collection2.DeleteMany(FilterDefinition<BsonDocument>.Empty);
+
+
+            collection2.InsertOne(new BsonDocument() { { "test", "zxcvzxcvzvczcxv" } });
+            collection1.InsertOne(new User() { Username = "rousojohn", Password = "123123123", Name = "Spyros" });
+
+            //var aaa = collection1.FindSync<BsonDocument>(FilterDefinition<User>.Empty).ToList();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Program.mainForm = new MainForm();
